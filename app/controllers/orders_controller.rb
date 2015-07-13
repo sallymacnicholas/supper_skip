e23e86e22a6d75ad7e5be6c3eaf7a4861942d2b2
Morgan Miller <m.miller722@gmail.com>
2015-07-13 17:14:32 -0600

    remove show and index from orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,8 @@
 class OrdersController < ApplicationController
   include OrderCreator
   
-  def show
-    @order = Order.find(params[:id])
-  end
-
   def new
     @order = Order.new
-  end
-
-  def index
-    if current_user
-      @orders = Order.where("user_id = ?", current_user.id)
-    else
-      redirect_to root_path
-    end
   end
 
   def create
