ea3c588446614bc3e703af316f6d9125758e34e0
Morgan Miller <m.miller722@gmail.com>
2015-07-14 23:12:01 -0600

    remove categories show

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,6 @@ Rails.application.routes.draw do
   end
 
   resources :restaurants, only: [:new, :create, :show], param: :slug do
-    # resources :categories, only: [:show]
     resources :items, only: [:show]
   end
 
