0733763a4f4957957777db2e4ccd5d094bd2f937
Morgan Miller <m.miller722@gmail.com>
2015-07-14 22:14:42 -0600

    add conditional to image view on root page

--- spec/integration/admin_can_manage_their_restaurant_spec.rb
+++ spec/integration/admin_can_manage_their_restaurant_spec.rb
@@ -19,7 +19,7 @@ describe "the owner" do
       .to receive(:current_user).and_return(owner)
 
     visit '/'
-
+    save_and_open_page
     within ".menu_right" do
       click_on "Manage Restaurant"
     end
