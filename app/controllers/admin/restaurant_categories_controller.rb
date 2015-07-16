523d27778a38009cc1f42b87f1a26baa1b7fe4c0
Morgan Miller <m.miller722@gmail.com>
2015-07-16 10:04:00 -0600

    delete old category functionality

--- app/controllers/admin/restaurant_categories_controller.rb
+++ app/controllers/admin/restaurant_categories_controller.rb
@@ -1,37 +1,54 @@
-class Admin::RestaurantCategoriesController < Admin::CategoriesController
+class Admin::RestaurantCategoriesController < ApplicationController
+  before_action :current_restaurant
+  before_action :find_category, only: [:edit, :update, :destroy]
+  
   def index
-    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
   end
 
   def new
-    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
     @category = Category.new
   end
-
+  
+  def edit
+  end
+  
   def create
-    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
-    @category = Category.new(category_params)
+    category = Category.new(category_params)
+    @restaurant.categories << category
     redirect_to admin_restaurant_categories_path(@restaurant)
-    @restaurant.categories << @category
   end
 
   def update
-    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
-    @category = Category.find(params[:id])
     @category.update(category_params)
     @category.save!
     redirect_to admin_restaurant_categories_path(@restaurant)
   end
 
   def destroy
-    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
-    @category = Category.find(params[:id])
-    @category.destroy
-    redirect_to admin_restaurant_categories_path(@restaurant)
+    if can_delete?
+      @category.destroy!
+      flash[:notice] = "You deleted the #{@category.name} category."
+      redirect_to admin_restaurant_categories_path(@restaurant)
+    else
+      flash[:warning] = "You cannot delete the #{@category.name} category, it still has items associated with it!"
+      redirect_to admin_restaurant_categories_path(@restaurant)
+    end
   end
   private
+  
+  def current_restaurant
+    @restaurant = Restaurant.find_by_slug(params[:restaurant_slug])
+  end
+  
+  def find_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
+  end
+  
+  def can_delete?
+    @category.category_items.empty?
   end
 end
